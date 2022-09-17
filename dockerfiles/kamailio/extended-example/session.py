6491a826b9ddf24ba2c113a9a76d861457a3afe5
JadKHaddad <jadkhaddad@gmail.com>
2022-09-17 11:27:13 +0200

    Using python processes to create more than one PJEndpoint and handle alot of calls

--- dockerfiles/kamailio/extended-example/session.py
+++ dockerfiles/kamailio/extended-example/session.py
@@ -2,6 +2,27 @@ from application import App, Account, CallerCall, SIP_HOST, SIP_PORT
 import pjsua2 as pj
 import logging
 import time
+from multiprocessing import Process
+
+
+def app(name, caller_usernaname, called_username):
+    app = App(sip_host=SIP_HOST, sip_port=SIP_PORT, name=name, run_time=10)
+    account_1 = Account(username=caller_usernaname, password=caller_usernaname)
+    account_2 = Account(username=called_username, password=called_username)
+    call = CallerCall(account_1, account_2.username)
+    time.sleep(3)
+    try:
+        call.makeCall(
+            f"sip:{account_2.username}@{SIP_HOST}:{SIP_PORT}", pj.CallOpParam()
+        )
+    except pj.Error as e:
+        logging.error(f"Exception: {e.status} {e.reason}")
+    except Exception as e:
+        logging.error(f"Exception: {e}")
+
+    app.handle_events()
+    app.destroy()
+
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -12,35 +33,19 @@ if __name__ == "__main__":
         level=logging.INFO,
     )
 
-    app = App(sip_host=SIP_HOST, sip_port=SIP_PORT, name="app1")
+    p1 = Process(target=app, args=("app1", "user1", "user2"))
+    p1.start()
 
-    account_1 = Account(username="user1", password="user1")
-    account_2 = Account(username="user2", password="user2")
-    # account_3 = Account(username="user3", password="user3")
-    # account_4 = Account(username="user4", password="user4")
-    # account_5 = Account(username="user5", password="user5")
-    # account_6 = Account(username="user6", password="user6")
-    # account_7 = Account(username="user7", password="user7")
-    # account_8 = Account(username="user8", password="user8")
+    p2 = Process(target=app, args=("app2", "user3", "user4"))
+    p2.start()
 
-    call_1 = CallerCall(account_1, account_2.username)
-    # call_3 = CallerCall(account_3, account_4.username)
-    # call_5 = CallerCall(account_5, account_6.username)
-    # call_7 = CallerCall(account_7, account_8.username)
+    p3 = Process(target=app, args=("app3", "user5", "user6"))
+    p3.start()
 
-    time.sleep(3)
-    try:
-        call_1.makeCall(
-            f"sip:{account_2.username}@{SIP_HOST}:{SIP_PORT}", pj.CallOpParam()
-        )
-        # call_3.makeCall(f"sip:{account_4.username}@{SIP_HOST}:{SIP_PORT}", pj.CallOpParam())
-        # call_5.makeCall(f"sip:{account_6.username}@{SIP_HOST}:{SIP_PORT}", pj.CallOpParam())
-        # call_7.makeCall(f"sip:{account_8.username}@{SIP_HOST}:{SIP_PORT}", pj.CallOpParam())
-    except pj.Error as e:
-        logging.error(f"Exception: {e.status} {e.reason}")
-    except Exception as e:
-        logging.error(f"Exception: {e}")
+    p4 = Process(target=app, args=("app4", "user7", "user8"))
+    p4.start()
 
-    app.handle_events()
-
-    app.destroy()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
